293db89f48648adfd2bde7b35a7d8b74080a26de
Shrey K <srkkadia@gmail.com>
2024-07-11 17:48:54 +0530

    init

---
+++ Basics/First.java
@@ -0,0 +1,15 @@
+public class First {
+	public static void main(String[] args) {
+		System.out.printf("Hello, World!");
+	}
+}
+
+/*
+here,
+System -> class
+out -> object of PrintStream
+println() -> method of PrintStream
+
+to compile	: javac First.java
+to run		: java First
+*/
